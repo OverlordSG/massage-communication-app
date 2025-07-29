7d979828ebed9066e29e5140145247a127f7ee24
OverlordSG <sgraziani@gmail.com>
2025-07-29 17:54:02 +0200

    Create craco.config.js

---
+++ frontend/frontend/craco.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  devServer: {
+    port: 3000,
+    open: false,
+  },
+};
